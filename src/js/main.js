a47d2e750dee0050650b0874dc7f8dc267eca51d
Vandon13 <vandon0013@gmail.com>
2021-02-27 19:35:09 +0600

    slider was made

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,48 @@
-var button = document.querySelector('#button');
-var modal  = document.querySelector('#modal');
-var close  = document.querySelector('#close');
+'use strict';
 
-button.addEventListener('click', function() {
-	modal.classList.add('modal_active');
+const button = document.querySelectorAll('.button');
+const modal  = document.querySelector('.modal-bg');
+const closeBtn  = document.querySelector('.modal__close');
+
+button.forEach(function (item) {
+	item.addEventListener('click', function name() {
+		modal.classList.add('modal-bg_active');
+	});
 });
 
-close.addEventListener('click', function() {
-	modal.classList.remove('modal_active');
-})
+closeBtn.addEventListener('click', function() {
+	modal.classList.remove('modal-bg_active');
+});
+
+// const mySwiper = new Swiper('.slider', {
+// 	slidesPerView: 3,
+// 	speed: 500,
+// 	navigation: {
+// 		nextEl: '.arrow-right',
+// 		prevEl: '.arrow-left'
+// 	},
+// 	autoplay: {
+// 		delay: 3000,
+// 	},
+// 	// pagination: {
+// 	// 	el: '.swiper-pagination',
+// 	// 	type: 'bullets',
+// 	// }
+// 	scrollbar: {
+// 		el: '.swiper-scrollbar',
+// 		draggable: true,
+// 	}
+// });
+
+const mySwiper = new Swiper('.portfolio__slider', {
+	slidesPerView: 3,
+	speed: 500,
+	navigation: {
+		nextEl: '.arrow-right',
+		prevEl: '.arrow-left'
+	},
+	pagination: {
+		el: '.portfolio__pagination',
+		clickable: 'true'
+	}
+});
