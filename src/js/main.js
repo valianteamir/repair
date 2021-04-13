6d3f14680a5824bbee37e43f589e3d6583d336fa
Vandon13 <vandon0013@gmail.com>
2021-04-13 16:30:00 +0600

    price section is done, bugs fixed

--- src/js/main.js
+++ src/js/main.js
@@ -46,7 +46,7 @@ const sliderMain = new Swiper('.portfolio__slider', {
 	watchOverflow: true,
 	spaceBetween: 0,
 	slidesPerGroup: 1,
-	centeredSlides: true,
+	centeredSlides: false,
 	initialSlide: 0,
 	slidesPerColumn: 1,
 	loop: false,
@@ -68,31 +68,31 @@ const sliderMain = new Swiper('.portfolio__slider', {
 	// 	shadowScale: 0.4,
 	// 	slideShadows: 0,
 	// }
-	effect: 'coverflow',
-	coverflowEffect: {
-		slideShadows: false,
-		modifier: 1,
-		rotate: 80,
-		stretch: 0,
-		depth: 800,
-	},
+	// effect: 'coverflow',
+	// coverflowEffect: {
+	// 	slideShadows: false,
+	// 	modifier: 1,
+	// 	rotate: 80,
+	// 	stretch: 0,
+	// 	depth: 800,
+	// },
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
 		},
-		670: {
+		768: {
 			slidesPerView: 2,
 		},
-		992: {
+		1200: {
 			slidesPerView: 3,
 		}
 	},
 	// preloadImages: false,
 	// lazy: true,
-	zoom: {
-		maxRatio: 3,
-		minRatio: 1,
-	},
+	// zoom: {
+	// 	maxRatio: 3,
+	// 	minRatio: 1,
+	// },
 	// thumbs: {
 	// 	swiper: {
 	// 		el: '.image-mini-slider',
