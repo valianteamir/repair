480c11f3a46387b5b1380634322c22e0d0442606
Vandon13 <vandon0013@gmail.com>
2021-04-17 12:02:15 +0600

    backup

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,17 @@
 const button = document.querySelectorAll('.button');
 const modal  = document.querySelector('.modal-bg');
 const closeBtn  = document.querySelector('.modal__close');
+const cardsHalfFirst = document.querySelectorAll('.card-half-row-1');
+const cardsHalfSecond = document.querySelectorAll('.card-half-row-2');
+const cardsThirdFirst = document.querySelectorAll('.card-third-row-1');
+const cardsThirdSecond = document.querySelectorAll('.card-third-row-2');
+const cardsThirdSquared = document.querySelectorAll('.card-third-row-3');
+const card1 = document.querySelector('.card-1');
+const card2 = document.querySelector('.card-2');
+const card3 = document.querySelector('.card-3');
+const card4 = document.querySelector('.card-4');
+const card5 = document.querySelector('.card-5');
+const card6 = document.querySelector('.card-6');
 
 button.forEach(function (item) {
 	item.addEventListener('click', function name() {
@@ -103,3 +114,50 @@ const sliderMain = new Swiper('.portfolio__slider', {
 
 // sliderMain.controller.control = textSlider;
 // textSlider.controller.control = sliderMain;
+
+window.addEventListener('scroll', function () {
+	let scrollTop = window.pageYOffset;
+	let windowWidth = document.documentElement.clientWidth;
+
+	if (windowWidth > 1200) {
+			if (scrollTop >= 1370) {
+		cardsHalfFirst.forEach(function (item) {
+			item.style.display = 'block';
+			item.classList.add('animate__animated', 'animate__fadeInUp');
+		});
+	}
+	if (scrollTop >= 1801) {
+		cardsHalfSecond.forEach(function (item) {
+			item.style.display = 'block';
+			item.classList.add('animate__animated', 'animate__fadeInUp');
+		});
+	}
+	}
+	if (windowWidth < 1200 && windowWidth > 992) {
+		if (scrollTop >= 2109) {
+			cardsThirdFirst.forEach(function (item) {
+				item.style.display = 'block';
+				item.style.animationDuration = '1s';
+				item.classList.add('animate__animated', 'animate__fadeInUp');
+		});
+	}
+		if (scrollTop >= 2610) {
+			cardsThirdSecond.forEach(function (item) {
+				item.style.display = 'block';
+				item.style.animationDuration = '1s';
+				item.classList.add('animate__animated', 'animate__fadeInUp');
+		});
+	}
+		if (scrollTop >= 3112) {
+			cardsThirdSquared.forEach(function (item) {
+				item.style.display = 'block';
+				item.style.animationDuration = '1s';
+				item.classList.add('animate__animated', 'animate__fadeInUp');
+			});
+	}
+	} else if (windowWidth < 992 && windowWidth > 768) {
+		
+	} else if (windowWidth < 768 && windowWidth > 576) {
+		
+	}
+});
